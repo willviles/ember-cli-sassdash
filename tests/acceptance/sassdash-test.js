af13c105265c72b47ed9994e7932e8b066ed5ac7
Alex DiLiberto <alex@alexdiliberto.com>
2017-06-13 00:30:35 -0400

    Adding basic acceptance test

---
+++ tests/acceptance/sassdash-test.js
@@ -0,0 +1,16 @@
+import { test } from 'qunit';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import { visit, find } from 'ember-native-dom-helpers';
+
+moduleForAcceptance('Acceptance | sassdash test');
+
+test('Sassdash is working', async function(assert) {
+  await visit('/');
+
+  let pseudoElemContent = window
+    .getComputedStyle(find('.test'), ':before')
+    .getPropertyValue('content')
+    .replace(/\"/g, '');
+
+  assert.equal(pseudoElemContent, 'sassdash is cool!');
+});
